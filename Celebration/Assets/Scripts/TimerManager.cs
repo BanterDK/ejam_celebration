44ae0ec46642456757b703cd220e7c94d7587b4f
jarrod10 <jarrodadair43@hotmail.com>
2020-06-27 23:55:14 +1000

    Timer started
    Timer script started and a couple other fixes

---
+++ Celebration/Assets/Scripts/TimerManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour
+{
+    [Header("Time in seconds")]
+    public float timeLimit;   //timeLimit is the amount of time for the game in minutes
+    float currentTime = 0.0f;
+
+    int minutesPassed = 0;
+    int secondsPassed = 0;
+
+    bool isCounting = false;
+    bool timeUp = false;        // timeUp bool used to measure wether the game is out of time or not
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isCounting = true;
+    }
+
+    void Update()
+    {
+        Debug.Log(currentTime);
+        minutesPassed = (int)(currentTime/60);
+        secondsPassed = (int)currentTime%60;
+        Debug.Log(minutesPassed + " minutes and " + secondsPassed + " seconds have passed");
+
+        if (currentTime >= timeLimit) {
+            timeUp = true;
+            isCounting = false;
+        }
+      
+        if (isCounting)
+        {
+            currentTime += Time.deltaTime; 
+        }
+    }
+}
