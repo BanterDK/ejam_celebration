44be039ed0b61c0ac7b73d74da4fd0e3f7761547
BanterDK <oliver.aiden.smith@gmail.com>
2020-06-29 05:00:46 +1000

    MainMenu Connected

---
+++ Celebration/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        //SceneManager
+        Application.Quit();
+    }
+
+}
