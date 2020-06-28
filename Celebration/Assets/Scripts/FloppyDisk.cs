dc0270d5251535e5de69e841c04ddf18c13b8dce
BanterDK <oliver.aiden.smith@gmail.com>
2020-06-28 13:47:02 +1000

    FloppyDisk Import

---
+++ Celebration/Assets/Scripts/FloppyDisk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloppyDisk : MonoBehaviour
+{
+    [Header("Stats")]
+    bool canBePickedUp;
+
+
+
+    //[Header("Effects")]
+    //ParticleSystem
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
